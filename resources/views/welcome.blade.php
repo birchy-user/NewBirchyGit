6f808da121d3d9db392b8227835ca8a32837415c
roberts pelle <roberts.pelle@gmail.com>
2019-06-23 02:25:28 +0300

    Roberts izveica izmainas 2:25

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    BirchyStore
+                    @lang('header.welcome')
                 </div>
 
                 <div class="links">
